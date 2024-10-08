759734bb202d5d27dd7b7392f75f5decea725057
Sivaprasad Galaba <sgalaba@tollplus.com>
2024-10-08 13:38:39 +0530

    commit-8

--- cypress/e2e/sample_test.cy.js
+++ cypress/e2e/sample_test.cy.js
@@ -5,7 +5,7 @@ describe('HTML Page Test', () => {
     });
 
     it('should display an h1 tag with the correct text', () => {
-       // cy.visit('http://localhost:8080/index.html');  // Adjust the path
+       cy.visit('http://localhost:8080/index.html');  // Adjust the path
         cy.log(cy.document().then((doc) => doc.documentElement.outerHTML));
         cy.get('h1').should('contain.text', 'hello codebegun');  // Increase the timeout
     });
